b14ba4bfbb80c095b397e294a171a97fd3cb81ff
Shreya N <nshreya094@gmail.com>
2024-05-26 23:51:34 +0530

    day19

---
+++ object.py
@@ -0,0 +1,14 @@
+
+from  prettytable import PrettyTable
+from turtle import Turtle, Screen
+
+# Create a Turtle object named timmy
+timmy = Turtle()
+print(timmy)
+print(timmy.shape("turtle"))
+print(timmy.color("coral"))
+print(timmy.forward(100))
+
+# Create a Screen object
+my_screen = Screen()
+print(my_screen.canvheight)
